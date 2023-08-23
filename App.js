a9b3a8347514d454566ce868a4bb6abac9ba6aca
imxamanda <amandavieiraperes.12@gmail.com>
2023-08-22 22:12:54 -0300

    criação de componentes

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
+import MeuComponente from './components/MeuComponente';
+import MinMax from './components/MinMax';
 
 export default function App() {
   return (
     <View style={styles.container}>
-  <Text style={styles.texto}>MOON</Text>
-  <Image
+    <ImageBackground source={require('./assets/rainbow.jpg')} style={styles.imageBackground}>
+    <Text style={styles.texto}>MOON</Text>
+    <Image
    style={styles.image}
-   source={require('./assets/lua.png')}
-   />
+   source={require('./assets/cloud.png')}
+    />
   
+  
+    <MeuComponente/>
+    <MinMax min='10' max='20'/>
     
+   </ImageBackground>
     </View>
   );
 }
@@ -18,19 +25,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#6959CD',
+    backgroundColor: '#C0C0C0',
     alignItems: 'center',
     justifyContent: 'center',
   },
 
   image: {
-    marginTop: 55,
     width: 300 ,
     height: 300
   },
 
   texto: {
     fontSize: 55,
+    alignItems: "center",
+    marginBottom: 50
     
+  },
+
+  imageBackground: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%"
   }
 });
---
+++ components/MinMax.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+
+
+export default function MinMax(props) {
+
+   const {min, max} = props;
+   console.warn(props);
+
+
+    return(
+        <>
+        <Text>{props.min}</Text>
+        <Text>{props.max}</Text>
+        </>
+    )
+}
